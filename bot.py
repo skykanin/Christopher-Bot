63a0d8c2ce4eefb84ccff88d25ce4c9fe04116e1
skykanin <3789764+skykanin@users.noreply.github.com>
2017-10-08 20:37:10 +0200

    Added unmute function and finished mute function

--- bot.py
+++ bot.py
@@ -9,7 +9,8 @@ bot_prefix = config["prefix"]
 
 client = commands.Bot(description=bot_description, command_prefix=bot_prefix)
 
-list_of_strings = ['add a reaction', 'add rxn', 'ok']
+list_of_strings = ['add a reaction', 'add rxn', 'ok', 'topkek']
+dict_of_roles = {} #user_id: rolesList[]
 
 @client.event
 async def on_ready():
@@ -33,12 +34,44 @@ async def mute(ctx):
     if hasRole and ctx.message.mentions:
         rolesToAdd = get_role(serverRoles, 'Muted')
         memberToMute = ctx.message.server.get_member(ctx.message.mentions[0].id)
+        try:
+            await client.remove_roles(memberToMute, *memberToMute.roles)
+        except discord.Forbidden:
+            return(await client.say("I don't have permission to do that! <:monkaS:356891254006611970>"))
+        dict_of_roles[memberToMute.id] = memberToMute.roles #Adds member to the mute list
+        print(dict_of_roles)
         await client.add_roles(memberToMute, rolesToAdd)
         return(await client.say("User {} is now muted".format(ctx.message.mentions[0])))
     elif hasRole and not ctx.message.mentions:
         return(await client.say("You haven't passed an argument. The command is: !mute <mentionUser>"))
     else:
-        return(await client.say("You don't have permission to use this command"))
+        return(await client.say("You don't have permission to use this command <:FeelsRageMan:356757133393657867>"))
+
+@client.command(pass_context=True)
+async def unmute(ctx):
+    hasRole = False
+    serverRoles = ctx.message.server.roles
+    for e in ctx.message.author.roles:
+        if e.name == "Admin":
+            hasRole = True
+    if hasRole and ctx.message.mentions:
+        roleToRemove = get_role(serverRoles, 'Muted')
+        memberToUnMute = ctx.message.server.get_member(ctx.message.mentions[0].id)
+        try:
+            rolesToAdd = dict_of_roles[memberToUnMute.id]
+        except KeyError:
+            return(await client.say("User isn't muted"))
+        try:
+            await client.remove_roles(memberToUnMute, roleToRemove)
+        except discord.Forbidden:
+            return(await client.say("I don't have permission to do that! <:monkaS:356891254006611970>"))
+        await client.add_roles(memberToUnMute, *rolesToAdd)
+        dict_of_roles.pop(memberToUnMute.id) #Removes member from the mute list
+        return(await client.say("User {} is now unmuted".format(ctx.message.mentions[0])))
+    elif hasRole and not ctx.message.mentions:
+        return(await client.say("You haven't passed an argument. The command is: !mute <mentionUser>"))
+    else:
+        return(await client.say("You don't have permission to use this command <:FeelsRageMan:356757133393657867>"))
 
 def get_role(server_roles, target_name):
     for each in server_roles:
