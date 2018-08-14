2c7c6cb9cd5d3a8fcd2c46ab6013e72bf65fcc29
skykanin <3789764+skykanin@users.noreply.github.com>
2018-08-14 02:23:18 +0200

    Updated config cog to rewrite

--- cogs/config.py
+++ cogs/config.py
@@ -44,38 +44,37 @@ class Config:
         conn.close()
         print("Deleted table") """
 
-    async def on_server_join(self, server):
+    async def on_guild_join(self, guild):
         # Set default values when entering new guild
         try:
-            self.add_default_values(server)
+            self.add_default_values(guild)
         except IntegrityError as e:
             print("Guild_settings already exists for this guild. IntegrityError", e)
         except OperationalError as e:
             print("OperationalError", e)
 
         # Prints the guild settings for specific guild
-        print(self.fetch_guild_settings(server))
+        print(self.fetch_guild_settings(guild))
 
-    def add_default_values(self, server):
+    def add_default_values(self, guild):
         # Choose the first text channel as default channel
-        for channel in server.channels:
-            if str(channel.type) == "text":
-                defualt = channel.name
-                break
+        for channel in guild.text_channels:
+            defualt = channel.name
+            break
 
         conn = sqlite3.connect(self.db)
         c = conn.cursor()
         with conn:
             c.execute("INSERT INTO {} VALUES (?,?,?,?,?,?)".format(self.table),
-            (server.id, server.name, False, defualt, defualt, 'Admin',))
+            (guild.id, guild.name, False, defualt, defualt, 'Admin',))
         conn.close()
 
     # Returns all the settings for a guild
-    def fetch_guild_settings(self, server):
+    def fetch_guild_settings(self, guild):
         conn = sqlite3.connect(self.db)
         c = conn.cursor()
         with conn:
-            c.execute("SELECT * FROM {} WHERE guild_id=?".format(self.table), (server.id,))
+            c.execute("SELECT * FROM {} WHERE guild_id=?".format(self.table), (guild.id,))
             guild_settings = c.fetchone()
 
             c.execute('PRAGMA TABLE_INFO({})'.format(self.table))
@@ -92,7 +91,7 @@ class Config:
         conn = sqlite3.connect(self.db)
         c = conn.cursor()
         with conn:
-            c.execute("SELECT {} FROM {} WHERE guild_id=?".format(self.settings['admin_role'], self.table), (ctx.message.server.id,))
+            c.execute("SELECT {} FROM {} WHERE guild_id=?".format(self.settings['admin_role'], self.table), (ctx.message.guild.id,))
             guild_role = c.fetchone()[0]
         conn.close()
 
@@ -103,25 +102,17 @@ class Config:
         return False
 
     # Checks if role or channel exists in guild
-    def check_if_exists(self, target_name, list_name, check_type):
-        # if we are checking channel
-        if check_type == 'channel':         
-            for element in list_name:
-                if target_name == element.name and str(element.type) == 'text':
-                    return True
-            return False
-        # if we are checking role
-        elif check_type == 'role':
-            for element in list_name:
-                if target_name == element.name:
-                    return True
-            return False
+    def check_if_exists(self, target_name, list_name):
+        for element in list_name:
+            if target_name == element.name:
+                return True
+        return False
 
     # Command for handling custom queries
     @commands.group(pass_context=True, invoke_without_command=True)
     async def query(self, ctx, *args):
         if ctx.invoked_subcommand is None and args == "":
-            return(await self.bot.say('Invalid subcommand for query passed...'))
+            return(await ctx.send('Invalid subcommand for query passed...'))
 
         if args[0] == 'select' or args[0] == "select".upper():
             query_type = 'get'
@@ -137,49 +128,49 @@ class Config:
                 try:
                     c.execute(query)
                     get = c.fetchall()
-                    return(await self.bot.say(get))
+                    return(await ctx.send(get))
                 except Exception as e:
-                    return(await self.bot.say(e))
+                    return(await ctx.send(e))
         else:
             with conn:
                 try:
                     c.execute(query)
-                    return(await self.bot.say("Query executed"))
+                    return(await ctx.send("Query executed"))
                 except Exception as e:
-                    return(await self.bot.say(e))
+                    return(await ctx.send(e))
 
     @query.command(pass_context=True)
     async def print_settings(self, ctx):
         # Check if user has permission to use this command
         if not self.check_for_admin_role(ctx):
-            return(await self.bot.say("You don't have permission to use this command"))
+            return(await ctx.send("You don't have permission to use this command"))
 
         # Gets guild settings from database
         try:
-            guild_settings = self.fetch_guild_settings(ctx.message.server)
+            guild_settings = self.fetch_guild_settings(ctx.message.guild)
         except Exception as e:
-            return(await self.bot.say("Exception:", e))
+            return(await ctx.send("Exception:", e))
 
         formated_guild_settings = ""
 
         for element in guild_settings:
             formated_guild_settings += element + "\n"
 
-        return(await self.bot.say("```sql\nSELECT * FROM '{0}' WHERE guild_id={1};\n\n{2}```".format(self.table, guild_settings[0][10:], formated_guild_settings)))
+        return(await ctx.send("```sql\nSELECT * FROM '{0}' WHERE guild_id={1};\n\n{2}```".format(self.table, guild_settings[0][10:], formated_guild_settings)))
 
     @query.command(pass_context=True)
     async def switch_commands(self, ctx):
         # Check if user has permission to use this command
         if not self.check_for_admin_role(ctx):
-            return(await self.bot.say("You don't have permission to use this command"))
+            return(await ctx.send("You don't have permission to use this command"))
             
         conn = sqlite3.connect(self.db)
         c = conn.cursor()
         with conn:
             try:
-                c.execute("SELECT commands_disabled FROM {} WHERE guild_id=?".format(self.table), (ctx.message.server.id,))
+                c.execute("SELECT commands_disabled FROM {} WHERE guild_id=?".format(self.table), (ctx.message.guild.id,))
             except Exception as e:
-                return(await self.bot.say("Exception:", e))
+                return(await ctx.send("Exception:", e))
 
             commands_disabled = c.fetchone()[0]
         
@@ -190,62 +181,62 @@ class Config:
 
         with conn:
             try:
-                c.execute("UPDATE {} SET commands_disabled=? WHERE guild_id=?".format(self.table), (commands_disabled, ctx.message.server.id,))
-                return(await self.bot.say("`commands_disabled` was switched to {}".format(commands_disabled)))
+                c.execute("UPDATE {} SET commands_disabled=? WHERE guild_id=?".format(self.table), (commands_disabled, ctx.message.guild.id,))
+                return(await ctx.send("`commands_disabled` was switched to {}".format(commands_disabled)))
             except Exception as e:
-                return(await self.bot.say("Exception:", e))
+                return(await ctx.send("Exception:", e))
         conn.close()
 
     
     @query.command(pass_context=True)
     async def update_roll_channel(self, ctx, channel : str):
         if not self.check_for_admin_role(ctx):
-            return(await self.bot.say("You don't have permission to use this command"))
-        elif not self.check_if_exists(channel, ctx.message.server.channels, 'channel'):
-            return(await self.bot.say("`{}` text channel doesn't exist in this server".format(channel)))
+            return(await ctx.send("You don't have permission to use this command"))
+        elif not self.check_if_exists(channel, ctx.message.guild.text_channels):
+            return(await ctx.send("`{}` text channel doesn't exist in this guild".format(channel)))
         
         conn = sqlite3.connect(self.db)
         c = conn.cursor()
         with conn:
             try:
-                c.execute("UPDATE {0} SET {1}=? WHERE {2}={3}".format(self.table, self.settings['roll_channel'], self.settings['guild_id'], ctx.message.server.id), (channel,))
-                return(await self.bot.say("`{0}` has been updated to `{1}`".format(self.settings['roll_channel'], channel)))
+                c.execute("UPDATE {0} SET {1}=? WHERE {2}={3}".format(self.table, self.settings['roll_channel'], self.settings['guild_id'], ctx.message.guild.id), (channel,))
+                return(await ctx.send("`{0}` has been updated to `{1}`".format(self.settings['roll_channel'], channel)))
             except Exception as e:
-                return(await self.bot.say("Exception", e))
+                return(await ctx.send("Exception", e))
         conn.close()
 
     @query.command(pass_context=True)
     async def update_osu_channel(self, ctx, channel : str):
         if not self.check_for_admin_role(ctx):
-            return(await self.bot.say("You don't have permission to use this command"))
-        elif not self.check_if_exists(channel, ctx.message.server.channels, 'channel'):
-            return(await self.bot.say("`{}` text channel doesn't exist in this server".format(channel)))
+            return(await ctx.send("You don't have permission to use this command"))
+        elif not self.check_if_exists(channel, ctx.message.guild.text_channels):
+            return(await ctx.send("`{}` text channel doesn't exist in this guild".format(channel)))
         
         conn = sqlite3.connect(self.db)
         c = conn.cursor()
         with conn:
             try:
-                c.execute("UPDATE {0} SET {1}=? WHERE {2}={3}".format(self.table, self.settings['osu_channel'], self.settings['guild_id'], ctx.message.server.id), (channel,))
-                return(await self.bot.say("`{0}` has been updated to `{1}`".format(self.settings['osu_channel'], channel)))
+                c.execute("UPDATE {0} SET {1}=? WHERE {2}={3}".format(self.table, self.settings['osu_channel'], self.settings['guild_id'], ctx.message.guild.id), (channel,))
+                return(await ctx.send("`{0}` has been updated to `{1}`".format(self.settings['osu_channel'], channel)))
             except Exception as e:
-                return(await self.bot.say("Exception", e))
+                return(await ctx.send("Exception", e))
         conn.close()
 
     @query.command(pass_context=True)
     async def update_admin_role(self, ctx, role : str):
         if not self.check_for_admin_role(ctx):
-            return(await self.bot.say("You don't have permission to use this command"))
-        elif not self.check_if_exists(role, ctx.message.server.roles, 'role'):
-            return(await self.bot.say("`{}` role doesn't exist in this server".format(role)))
+            return(await ctx.send("You don't have permission to use this command"))
+        elif not self.check_if_exists(role, ctx.message.guild.roles):
+            return(await ctx.send("`{}` role doesn't exist in this guild".format(role)))
         
         conn = sqlite3.connect(self.db)
         c = conn.cursor()
         with conn:
             try:
-                c.execute("UPDATE {0} SET {1}=? WHERE {2}={3}".format(self.table, self.settings['admin_role'], self.settings['guild_id'], ctx.message.server.id), (role,))
-                return(await self.bot.say("`{0}` has been updated to `{1}`".format(self.settings['admin_role'], role)))
+                c.execute("UPDATE {0} SET {1}=? WHERE {2}={3}".format(self.table, self.settings['admin_role'], self.settings['guild_id'], ctx.message.guild.id), (role,))
+                return(await ctx.send("`{0}` has been updated to `{1}`".format(self.settings['admin_role'], role)))
             except Exception as e:
-                return(await self.bot.say("Exception", e))
+                return(await ctx.send("Exception", e))
         conn.close()
 
 def setup(bot):
